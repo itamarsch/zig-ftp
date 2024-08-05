968740c16a4c7dc993198177b8074693b31e3832
Itamar Schwartz <itamarschwartz10@gmail.com>
2024-08-05 18:20:29 +0300

    FTP initial structure

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,26 @@
 const std = @import("std");
+const mem = @import("mem");
 const root = @import("root.zig");
 
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const allocator = std.heap.page_allocator;
 
-    std.log.info("Run `zig build test` to run the tests.", .{});
-    std.log.info("Add: {d}", .{root.add(1, 2)});
+    var conn = try std.net.tcpConnectToHost(allocator, "test.rebex.net", 21);
+    try conn.writeAll("USER demo\r\n");
+    try conn.writeAll("PASS password\r\n");
+    var read_thread = try std.Thread.spawn(.{}, readFromStream, .{ allocator, &conn });
+
+    read_thread.join();
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn readFromStream(allocator: std.mem.Allocator, conn: *std.net.Stream) !void {
+    const buf = allocator.alloc(u8, 1024) catch unreachable;
+    defer allocator.free(buf);
+
+    while (true) {
+        const bytes_read = try conn.read(buf);
+        if (bytes_read == 0) break; // Connection closed
+
+        std.debug.print("{s}", .{buf[0..bytes_read]});
+    }
 }
